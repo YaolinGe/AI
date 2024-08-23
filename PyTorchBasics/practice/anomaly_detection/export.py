165dbbe459dbbc4b88ffa1c40bcf315b85a86423
Yaolin Ge <geyaolin@gmail.com>
2024-08-23 17:08:18 +0200

    update anomaly detection multi channel

--- PyTorchBasics/practice/anomaly_detection/export.py
+++ PyTorchBasics/practice/anomaly_detection/export.py
@@ -1,40 +1,77 @@
 import torch 
 import torch.nn as nn
-import torch.optim as optim
-import numpy as np
-import os 
-from preprocessing import preprocessing, create_sequences
-from model.LSTMAutoEncoder import LSTMAutoEncoder
-from training import training_loop
+
+class Encoder(nn.Module): 
+    def __init__(self): 
+        super().__init__()
+        self.lstm1 = nn.LSTM(
+            input_size=7, 
+            hidden_size=128, 
+            num_layers=1, 
+            batch_first=True
+        )
+        self.lstm2 = nn.LSTM(
+            input_size=128, 
+            hidden_size=16, 
+            num_layers=1, 
+            batch_first=True
+        )
+    
+    def forward(self, x):
+        x, (hidden, cell) = self.lstm1(x)
+        x, (hidden, cell) = self.lstm2(x)
+        return x
+    
+class Decoder(nn.Module): 
+    def __init__(self): 
+        super().__init__()
+        self.lstm1 = nn.LSTM(
+            input_size=16, 
+            hidden_size=128, 
+            num_layers=1, 
+            batch_first=True
+        )
+        self.lstm2 = nn.LSTM(
+            input_size=128, 
+            hidden_size=7, 
+            num_layers=1, 
+            batch_first=True
+        )
+
+    def forward(self, x):
+        x, (hidden, cell) = self.lstm1(x)
+        x, (hidden, cell) = self.lstm2(x)
+        # x = self.fc(x)
+        return x
+
+class LSTMAutoEncoder(nn.Module): 
+    def __init__(self): 
+        super().__init__()
+        self.encoder = Encoder()
+        self.decoder = Decoder()
+    
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+    
+
+model = LSTMAutoEncoder()
+model.load_state_dict(torch.load('modelV2.pth'))
+model.eval()
+
+torch.manual_seed(0)
+dummy_input = torch.randn(1, 30, 7)
 
 
-filepath = os.path.join(".", "data", "YaoBox", "Box2StrainRaw1.csv")
-t, value = preprocessing(filepath)
+# torch.onnx.export(model, dummy_input, 'lstm_autoencoder.onnx', opset_version=11)
 
-train_size = .6
-val_size = .2
-test_size = .2
-train_data = value[:int(len(value) * train_size)]
-val_data = value[int(len(value) * train_size):int(len(value) * (train_size + val_size))]
-test_data = value[int(len(value) * (train_size + val_size)):]
-
-window_size = 50
-train_sequences = create_sequences(train_data, window_size)
-val_sequences = create_sequences(val_data, window_size)
-test_sequences = create_sequences(test_data, window_size)
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-train_dataset = torch.tensor(train_sequences, dtype=torch.float32).unsqueeze(-1).to(device)
-val_dataset = torch.tensor(val_sequences, dtype=torch.float32).unsqueeze(-1).to(device)
-test_dataset = torch.tensor(test_sequences, dtype=torch.float32).unsqueeze(-1).to(device)
-
-print(train_dataset.shape, val_dataset.shape, test_dataset.shape)
-
-model = LSTMAutoEncoder().to(device)
-loss_fn = nn.MSELoss()
-optimizer = optim.Adam(model.parameters(), lr=1e-3)
-model
-
-train_loss, val_loss, accuracy = training_loop(model, train_dataset, val_dataset, optimizer, loss_fn, epochs=100)
+torch.onnx.export(
+    model,
+    dummy_input,
+    'anomalyDetectionMultiChannel.onnx',
+    opset_version=11,
+    input_names=['input'],   # Specify the input name
+    output_names=['output']  # Specify the output name
+)
 
