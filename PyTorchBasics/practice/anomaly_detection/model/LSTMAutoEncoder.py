3a08255ecf764550ea1a15c3b5cf4ab85e71de1f
Yaolin Ge <geyaolin@gmail.com>
2024-08-28 15:48:48 +0200

    update integration

--- PyTorchBasics/practice/anomaly_detection/model/LSTMAutoEncoder.py
+++ PyTorchBasics/practice/anomaly_detection/model/LSTMAutoEncoder.py
@@ -7,58 +7,55 @@ import torch
 import torch.nn as nn
 
 
-class Encoder(nn.Module):
-    def __init__(self):
+class Encoder(nn.Module): 
+    def __init__(self): 
         super().__init__()
         self.lstm1 = nn.LSTM(
-            input_size=1,
-            hidden_size=64,
-            num_layers=1,
+            input_size=7, 
+            hidden_size=128, 
+            num_layers=1, 
             batch_first=True
         )
         self.lstm2 = nn.LSTM(
-            input_size=64,
-            hidden_size=16,
-            num_layers=1,
+            input_size=128, 
+            hidden_size=16, 
+            num_layers=1, 
+            batch_first=True
+        )
+    
+    def forward(self, x):
+        x, (hidden, cell) = self.lstm1(x)
+        x, (hidden, cell) = self.lstm2(x)
+        return x
+    
+class Decoder(nn.Module): 
+    def __init__(self): 
+        super().__init__()
+        self.lstm1 = nn.LSTM(
+            input_size=16, 
+            hidden_size=128, 
+            num_layers=1, 
+            batch_first=True
+        )
+        self.lstm2 = nn.LSTM(
+            input_size=128, 
+            hidden_size=7, 
+            num_layers=1, 
             batch_first=True
         )
 
     def forward(self, x):
         x, (hidden, cell) = self.lstm1(x)
         x, (hidden, cell) = self.lstm2(x)
+        # x = self.fc(x)
         return x
 
-
-class Decoder(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.lstm1 = nn.LSTM(
-            input_size=16,
-            hidden_size=64,
-            num_layers=1,
-            batch_first=True
-        )
-        self.lstm2 = nn.LSTM(
-            input_size=64,
-            hidden_size=1,
-            num_layers=1,
-            batch_first=True
-        )
-        self.fc = nn.Linear(1, 1)
-
-    def forward(self, x):
-        x, (hidden, cell) = self.lstm1(x)
-        x, (hidden, cell) = self.lstm2(x)
-        x = self.fc(x)
-        return x
-
-
-class LSTMAutoEncoder(nn.Module):
-    def __init__(self):
+class LSTMAutoEncoder(nn.Module): 
+    def __init__(self): 
         super().__init__()
         self.encoder = Encoder()
         self.decoder = Decoder()
-
+    
     def forward(self, x):
         x = self.encoder(x)
         x = self.decoder(x)
