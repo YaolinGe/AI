be30342ef24f35043399a0b40e925805417ae747
Yaolin Ge <geyaolin@gmail.com>
2024-11-14 15:22:55 +0100

    finished disk 1 data merging

--- deeplearningwithpytorch/practice/DataAnalysisApp/merge_missy_datasets.py
+++ deeplearningwithpytorch/practice/DataAnalysisApp/merge_missy_datasets.py
@@ -9,13 +9,13 @@ import os
 
 if __name__ == "__main__": 
     try:
-        missy_dir = r"C:\Data\MissyDataSet\Missy_Disc2\Cutfiles"
+        missy_dir = r"C:\Data\MissyDataSet\Missy_Disc1\Cutfiles"
         # missy_dir = r"C:\Data\MissyDataSet"
         filenames = os.listdir(missy_dir)
         filenames = [os.path.join(missy_dir, filename) for filename in filenames if filename.endswith('.cut')]
 
         cut_file_merger = CutFileMerger()
-        cut_file_merger.merge_cut_files(filenames, resolution_ms=100, filepath=os.path.join("datasets", "df_missy_disk2.csv"))
+        cut_file_merger.merge_cut_files(filenames, resolution_ms=100, filepath=os.path.join("datasets", "df_missy_disk1.csv"))
 
     except Exception as e:
         print(f"Error: {e}")
